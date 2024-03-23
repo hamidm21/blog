6de84238324fd39022a78414b6f5d857fd4630b3
alizahedi <alizahedi499@gmail.com>
2024-03-23 22:25:42 +0330

    Add test cases for factories in tests.py

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .factories import CommentFactory, PostFactory
+from .models import Comment, Post
 
 # Create your tests here.
+
+class PostModelTestCase(TestCase):
+    def test_post_creation(self):
+        post = PostFactory()
+        self.assertIsInstance(post, Post)
+
+class CommentModelTestCase(TestCase):
+    def test_comment_creation(self):
+        comment = CommentFactory()
+        self.assertIsInstance(comment, Comment)
