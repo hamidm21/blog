f6b7b64b99e88e415e82f37478e2655fe7f3a3dc
alizahedi <alizahedi499@gmail.com>
2024-03-23 21:00:31 +0330

    Create 'Comment' model

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,16 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"Comment by {self.email} on {self.post}"
+    
+    
