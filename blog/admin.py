8a32f2a4170fc516d6766d315303e79e5998c337
alizahedi <alizahedi499@gmail.com>
2024-03-23 21:45:47 +0330

    Register 'Post' model in admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post, Comment
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('title', 'content')
