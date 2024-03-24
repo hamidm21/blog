e972bfd437386cc89da90d9849f9871bdecf3304
alizahedi <alizahedi499@gmail.com>
2024-03-25 02:12:14 +0330

    Add URL pattern for creating a comment API endpoint

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListAPIView, CommentListAPIView, PostRetrieveAPIView, PostCreateAPIView
+from .views import PostListAPIView, CommentListAPIView, PostRetrieveAPIView, PostCreateAPIView, CommentCreateAPIView
 
 urlpatterns = [
     path('posts/', PostListAPIView.as_view(), name='post-list'),
     path('comments/', CommentListAPIView.as_view(), name='comment-list'),
     path('posts/<int:pk>/', PostRetrieveAPIView.as_view(), name='post-detail'),
     path('posts/create/', PostCreateAPIView.as_view(), name='post-create'),
+    path('comments/create/', CommentCreateAPIView.as_view(), name='comment-create'),
 ]
