ec0f05b2a9c7ad18e629f183156f439ccdfa6efd
alizahedi <alizahedi499@gmail.com>
2024-03-25 02:24:08 +0330

    Add URL pattern for updating a post API endpoint

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListAPIView, CommentListAPIView, PostRetrieveAPIView, PostCreateAPIView, CommentCreateAPIView
+from .views import PostListAPIView, CommentListAPIView, PostRetrieveAPIView, PostCreateAPIView, CommentCreateAPIView, PostUpdateAPIView
 
 urlpatterns = [
     path('posts/', PostListAPIView.as_view(), name='post-list'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('posts/<int:pk>/', PostRetrieveAPIView.as_view(), name='post-detail'),
     path('posts/create/', PostCreateAPIView.as_view(), name='post-create'),
     path('comments/create/', CommentCreateAPIView.as_view(), name='comment-create'),
+    path('posts/<int:pk>/update/', PostUpdateAPIView.as_view(), name='post-update'),
 ]
