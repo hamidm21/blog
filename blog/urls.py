050963545cc9094f47759ea3608f930d6a955575
alizahedi <alizahedi499@gmail.com>
2024-03-25 01:48:26 +0330

    Add URL pattern for retrieving a single specific post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostListAPIView, CommentListAPIView
+from .views import PostListAPIView, CommentListAPIView, PostRetrieveAPIView
 
 urlpatterns = [
     path('posts/', PostListAPIView.as_view(), name='post-list'),
     path('comments/', CommentListAPIView.as_view(), name='comment-list'),
+    path('posts/<int:pk>/', PostRetrieveAPIView.as_view(), name='post-detail'),
 ]
