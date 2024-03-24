94168d28717867354d53c7a668c6477c5c585a1a
alizahedi <alizahedi499@gmail.com>
2024-03-25 01:39:57 +0330

    Add URL pattern for Comment list API endpoint

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostListAPIView
+from .views import PostListAPIView, CommentListAPIView
 
 urlpatterns = [
     path('posts/', PostListAPIView.as_view(), name='post-list'),
+    path('comments/', CommentListAPIView.as_view(), name='comment-list'),
 ]
