959f3cb2a8d1f467b80def4570cf1c98425aa536
alizahedi <alizahedi499@gmail.com>
2024-03-25 01:55:34 +0330

    Add URL pattern for creating a post API endpoint

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostListAPIView, CommentListAPIView, PostRetrieveAPIView
+from .views import PostListAPIView, CommentListAPIView, PostRetrieveAPIView, PostCreateAPIView
 
 urlpatterns = [
     path('posts/', PostListAPIView.as_view(), name='post-list'),
     path('comments/', CommentListAPIView.as_view(), name='comment-list'),
     path('posts/<int:pk>/', PostRetrieveAPIView.as_view(), name='post-detail'),
+    path('posts/create/', PostCreateAPIView.as_view(), name='post-create'),
 ]
