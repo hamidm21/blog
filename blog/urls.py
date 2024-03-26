3543468830ba130f8849f550c4aebadd8fd3d1b1
alizahedi <alizahedi499@gmail.com>
2024-03-27 03:01:54 +0330

    Include URL router for ViewSet API in urlpatterns

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import PostListAPIView, CommentListAPIView, PostRetrieveAPIView, PostCreateAPIView, CommentCreateAPIView, PostUpdateAPIView
+from rest_framework.routers import DefaultRouter
+from .views import PostListAPIView, CommentListAPIView, PostRetrieveAPIView, PostCreateAPIView, CommentCreateAPIView, PostUpdateAPIView, PostViewSet
+
+router = DefaultRouter()
+router.register(r'articles', PostViewSet)
 
 urlpatterns = [
     path('posts/', PostListAPIView.as_view(), name='post-list'),
@@ -9,3 +13,5 @@ urlpatterns = [
     path('comments/create/', CommentCreateAPIView.as_view(), name='comment-create'),
     path('posts/<int:pk>/update/', PostUpdateAPIView.as_view(), name='post-update'),
 ]
+
+urlpatterns += router.urls
